7ae0505df0e28ed9e41463a38e95f76d0975c8db
iNT2k <joerg+kennungen.github@dorgeist.com>
2018-01-08 06:35:48 +0100

    Some Documentation

--- perl/jdpix.pm
+++ perl/jdpix.pm
@@ -10,15 +10,15 @@ sub new {
 	$socket->autoflush(1);
 	my $arr=();
 	my $self={
-		host => $args->{'host'},
-		port => $args->{'port'},
-		leds => $args->{'leds'},
-		socket => $socket,
-		packetsize => 1400,
-		arr => $arr
+		host => $args->{'host'},	# Host or IP of the ESP
+		port => $args->{'port'},	# Port of the ESP
+		leds => $args->{'leds'},	# Count of LEDs (should match to NUM_LEDS in Sketch)
+		socket => $socket,		# Holds the Socket
+		packetsize => 1400,		# Packetsize (must match to packetsize in Sketch)
+		arr => $arr			# LED-Array
 	};
 	bless($self,$class);
-	$self->init_arr();
+	$self->init_arr();			# initialise the Array once
 	return($self);
 }
 
@@ -29,7 +29,7 @@ sub disconnect {
 	}
 }
 
-sub confetti {
+sub confetti {					# Demo
 	my ($self,$looper)=@_;
 	$looper++;
 	my $rand_led=int(rand($self->{'leds'}));
@@ -39,7 +39,7 @@ sub confetti {
 }
 
 
-sub fade2black {
+sub fade2black {				# Fades all LEDs down by $amount (works for HSV and RGB)
 	my($self,$amount)=@_;
 	for (my $i=0;$i<=$self->{'leds'};$i++) {
 		if ($self->{'arr'}->[$i]->[3]==1) {
@@ -53,7 +53,7 @@ sub fade2black {
 }
 
 
-sub set_rgb {
+sub set_rgb {					# Set RGB-LED $led to $g $g and $b
 	my ($self,$led,$r,$g,$b)=@_;
 	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[3]=0;
 	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[0]=$r;
@@ -61,15 +61,15 @@ sub set_rgb {
 	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[2]=$b;
 }
 
-sub set_hsv {
-	my ($self,$led,$r,$g,$b)=@_;
+sub set_hsv {					# Set HSV $led to $h $s $v
+	my ($self,$led,$h,$s,$v)=@_;
 	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[3]=1;
-	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[0]=$r;
-	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[1]=$g;
-	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[2]=$b;
+	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[0]=$h;
+	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[1]=$s;
+	$self->{'arr'}->[$led%($self->{'leds'}+1)]->[2]=$v;
 }
 
-sub init_arr {
+sub init_arr {					# Initialise the whole Array with black
 	my ($self)=@_;
 	for (my $i=0;$i<=$self->{'leds'};$i++) {
 		$self->{'arr'}->[$i]->[3]=0;
@@ -79,25 +79,25 @@ sub init_arr {
 	}
 }
 
-sub show {
+sub show {					# Alias for send_it
 	my ($self)=@_;
 	$self->send_it();
 }
 
-sub send_it {
+sub send_it {					# Transmits Array to ESP
 	my ($self)=@_;
 	$self->{'frames'}++;
 	my $message='';
 	for (my $i=0;$i<$self->{'leds'};$i++) {
-		$message.=pack("C",$i);
-		$message.=pack("C",$self->{'arr'}->[$i]->[3]);
-		$message.=pack("C",$self->{'arr'}->[$i]->[0]);
-		$message.=pack("C",$self->{'arr'}->[$i]->[1]);
-		$message.=pack("C",$self->{'arr'}->[$i]->[2]);
+		$message.=pack("C",$i);					# 1st Byte: # of LED
+		$message.=pack("C",$self->{'arr'}->[$i]->[3]);		# 2nd Byte: RGB (0) or HSV (1)
+		$message.=pack("C",$self->{'arr'}->[$i]->[0]);		# 3rd Byte: Red (when RGB) or Hue (when HSV)
+		$message.=pack("C",$self->{'arr'}->[$i]->[1]);		# 4th Byte: Green (when RGB) or Saturation (when HSV)
+		$message.=pack("C",$self->{'arr'}->[$i]->[2]);		# 5th Byte: Blue (when RGB) or Value (when HSV)
 	}
-	$message.=chr(00) while length($message)<$self->{'packetsize'};
-	$self->{'socket'}->say($message);
-	$self->{'socket'}->flush();
+	$message.=chr(00) while length($message)<$self->{'packetsize'};	# Fill Packet to packetsize
+	$self->{'socket'}->say($message);				# Send via UDP
+	$self->{'socket'}->flush();					# Flush it (no delay!)
 }
 
 return 1;
